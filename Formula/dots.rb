5bc7f0953d7a764ce9298150a1bb8d3d51a29747
Joel Reymont <18791+joelreymont@users.noreply.github.com>
2026-01-19 14:50:01 +0200

    Bump dots to 0.6.4

--- Formula/dots.rb
+++ Formula/dots.rb
@@ -6,8 +6,8 @@ class Dots < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/joelreymont/dots/releases/download/v0.6.3/dot-macos-arm64"
-      sha256 "af8b8613c51484263f63b4236e6867b512c40cf41ed0aec0fba8eccd1ddea105"
+      url "https://github.com/joelreymont/dots/releases/download/v0.6.4/dot-macos-arm64"
+      sha256 "e60ec762d45e3f4e07f58d06b4456f5c84e67960b32fe48d1ecce50aeb1c3b67"
     end
     on_intel do
       odie "Intel Mac binaries not available. Please build from source."
@@ -16,8 +16,8 @@ class Dots < Formula
 
   on_linux do
     on_intel do
-      url "https://github.com/joelreymont/dots/releases/download/v0.6.3/dot-linux-x86_64"
-      sha256 "3bcee6f683b58d2c48b42c81c1a1a9ea3652e4cb31bed19053b536126aa80e97"
+      url "https://github.com/joelreymont/dots/releases/download/v0.6.4/dot-linux-x86_64"
+      sha256 "26bd0d35af76d6a16b7a056229b5579604984c8deb52d673d387b54faa5efa5c"
     end
   end
 
